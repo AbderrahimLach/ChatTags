cd156c9091e2ba48e50bd154c79217a46b037bcf
AbderrahimLach$ <serversreport15@gmail.com>
2021-11-19 17:47:55 +0100

    Code cleanup

--- src/main/java/com/abderrahimlach/listeners/MenuListener.java
+++ src/main/java/com/abderrahimlach/listeners/MenuListener.java
@@ -3,6 +3,7 @@ package com.abderrahimlach.listeners;
 import com.abderrahimlach.TagPlugin;
 import com.abderrahimlach.management.MenuManager;
 import com.abderrahimlach.tag.inventory.InventoryUI;
+import lombok.Data;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,12 +13,10 @@ import org.bukkit.event.inventory.InventoryCloseEvent;
 /**
  * @author AbderrahimLach
  */
+@Data
 public class MenuListener implements Listener {
 
     private final MenuManager menuManager;
-    public MenuListener(TagPlugin plugin){
-        this.menuManager = plugin.getMenuManager();
-    }
 
     @EventHandler
     public void onClick(InventoryClickEvent event){
